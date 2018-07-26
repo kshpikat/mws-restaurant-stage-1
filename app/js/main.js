5b26489b32ec3979f9603d3e86be0a621595ee07
Constantine Shpikat <cshpikat@sugarcrm.com>
2018-07-26 18:17:14 +0200

    Webpack

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,11 @@
+import { oneLineTrim } from 'common-tags';
+import "../css/styles.css";
+import "normalize.css/normalize.css";
+
 import loadGoogleMapsApi from "load-google-maps-api";
 import { DBHelper, isHome, titleGoogleMap } from './dbhelper';
 import LazyLoad from './lazyload.es2015';
 
-// import { DBHelper, titleGoogleMap, isHome, setLazyImageLoadObserver} from './dbhelper.js';
 
 // if ('serviceWorker' in navigator) {
 //     window.addEventListener('load', function () {
@@ -10,46 +13,20 @@ import LazyLoad from './lazyload.es2015';
 //     });
 // }
 
-
-let restaurants,
-  neighborhoods,
-  cuisines;
-var map;
-var markers = [];
-
-
-/**
- * Fetch neighborhoods and cuisines as soon as the page is loaded.
- */
-if(isHome()) {
-  document.addEventListener('DOMContentLoaded', () => {
-    fetchNeighborhoods();
-    fetchCuisines();
-    document.getElementById('neighborhoods-select').addEventListener('change', updateRestaurants);
-    document.getElementById('cuisines-select').addEventListener('change', updateRestaurants);
-  });
-}
-
-/**
- * Fetch all neighborhoods and set their HTML.
- */
-const fetchNeighborhoods = () => {
-  DBHelper.fetchNeighborhoods((error, neighborhoods) => {
-    if (error) { // Got an error
-      console.error(error);
-    } else {
-      self.neighborhoods = neighborhoods;
-      fillNeighborhoodsHTML();
-    }
-  });
-};
+let globalLazyLoad;
+let globalRestaurants;
+let globalNeighborhoods;
+let globalCuisines;
+let globalMap;
+let markers = [];
+let globalMapLoaded;
 
 /**
  * Set neighborhoods HTML.
  */
-const fillNeighborhoodsHTML = (neighborhoods = self.neighborhoods) => {
+const fillNeighborhoodsHTML = (newItems = globalNeighborhoods) => {
   const select = document.getElementById('neighborhoods-select');
-  neighborhoods.forEach(neighborhood => {
+  newItems.forEach((neighborhood) => {
     const option = document.createElement('option');
     option.innerHTML = neighborhood;
     option.value = neighborhood;
@@ -58,26 +35,12 @@ const fillNeighborhoodsHTML = (neighborhoods = self.neighborhoods) => {
 };
 
 /**
- * Fetch all cuisines and set their HTML.
- */
-const fetchCuisines = () => {
-  DBHelper.fetchCuisines((error, cuisines) => {
-    if (error) { // Got an error!
-      console.error(error);
-    } else {
-      self.cuisines = cuisines;
-      fillCuisinesHTML();
-    }
-  });
-};
-
-/**
  * Set cuisines HTML.
  */
-const fillCuisinesHTML = (cuisines = self.cuisines) => {
+const fillCuisinesHTML = (inboundCuisines = globalCuisines) => {
   const select = document.getElementById('cuisines-select');
 
-  cuisines.forEach(cuisine => {
+  inboundCuisines.forEach((cuisine) => {
     const option = document.createElement('option');
     option.innerHTML = cuisine;
     option.value = cuisine;
@@ -86,75 +49,20 @@ const fillCuisinesHTML = (cuisines = self.cuisines) => {
 };
 
 /**
- * Initialize Google map, called from HTML.
- */
-if(isHome()) {
-  window.initMap = () => {
-    let loc = {
-      lat: 40.722216,
-      lng: -73.987501
-    };
-    const map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 12,
-      center: loc,
-      scrollwheel: false
-    });
-    titleGoogleMap(map, 'Restaurants Map');
-    self.map = map;
-
-    updateRestaurants();
-  };
-}
-/**
- * Update page and map for current restaurants.
- */
-const updateRestaurants = () => {
-  const cSelect = document.getElementById('cuisines-select');
-  const nSelect = document.getElementById('neighborhoods-select');
-
-  const cIndex = cSelect.selectedIndex;
-  const nIndex = nSelect.selectedIndex;
-
-  const cuisine = cSelect[cIndex].value;
-  const neighborhood = nSelect[nIndex].value;
-
-  DBHelper.fetchRestaurantByCuisineAndNeighborhood(cuisine, neighborhood, (error, restaurants) => {
-    if (error) { // Got an error!
-      console.error(error);
-    } else {
-      resetRestaurants(restaurants);
-      fillRestaurantsHTML();
-    }
-  });
-};
-
-/**
  * Clear current restaurants, their HTML and remove their map markers.
  */
-const resetRestaurants = (restaurants) => {
+const resetRestaurants = (newItems) => {
   // Remove all restaurants
-  self.restaurants = [];
+  globalRestaurants = [];
   const ul = document.getElementById('restaurants-list');
   ul.innerHTML = '';
 
   // Remove all map markers
-  if(self.markers && self.markers instanceof Array) {
-    self.markers.forEach(m => m.setMap(null));
+  if (markers && markers instanceof Array) {
+    markers.forEach(m => m.setMap(null));
   }
-  self.markers = [];
-  self.restaurants = restaurants;
-};
-
-/**
- * Create all restaurants HTML and add them to the webpage.
- */
-const fillRestaurantsHTML = (restaurants = self.restaurants) => {
-  const ul = document.getElementById('restaurants-list');
-  restaurants.forEach(restaurant => {
-    ul.append(createRestaurantHTML(restaurant));
-  });
-  addMarkersToMap();
-  new LazyLoad();
+  markers = [];
+  globalRestaurants = newItems;
 };
 
 /**
@@ -165,7 +73,10 @@ const createRestaurantHTML = (restaurant) => {
 
   const image = document.createElement('img');
   image.className = 'restaurant-img lazyload';
-  image.setAttribute('data-srcset', DBHelper.imageSetUrlForRestaurant(restaurant));
+  const img = require(`../img/${restaurant.id}.jpg`);
+  image.src = img.placeholder;
+  image.setAttribute('data-src', img.src);
+  image.setAttribute('data-srcset', img.srcSet);
   image.alt = `Photo for ${restaurant.name}`;
   li.append(image);
 
@@ -192,13 +103,162 @@ const createRestaurantHTML = (restaurant) => {
 /**
  * Add markers for current restaurants to the map.
  */
-const addMarkersToMap = (restaurants = self.restaurants) => {
-  restaurants.forEach(restaurant => {
+const addMarkersToMap = (newItems = globalRestaurants) => {
+  addInteractiveMap();
+  newItems.forEach((restaurant) => {
     // Add marker to the map
-    const marker = DBHelper.mapMarkerForRestaurant(restaurant, self.map);
+    const marker = DBHelper.mapMarkerForRestaurant(restaurant, globalMap);
     google.maps.event.addListener(marker, 'click', () => {
       window.location.href = marker.url;
     });
-    self.markers.push(marker);
+    markers.push(marker);
   });
 };
+
+/**
+ * Create all restaurants HTML and add them to the webpage.
+ */
+const fillRestaurantsHTML = (newItems = globalRestaurants) => {
+  const ul = document.getElementById('restaurants-list');
+  newItems.forEach((restaurant) => {
+    ul.append(createRestaurantHTML(restaurant));
+  });
+  addMarkersToMap();
+  globalLazyLoad = new LazyLoad();
+};
+
+/**
+ * Update page and map for current restaurants.
+ */
+const updateRestaurants = () => {
+  const cSelect = document.getElementById('cuisines-select');
+  const nSelect = document.getElementById('neighborhoods-select');
+
+  const cIndex = cSelect.selectedIndex;
+  const nIndex = nSelect.selectedIndex;
+
+  const cuisine = cSelect[cIndex].value;
+  const neighborhood = nSelect[nIndex].value;
+
+  DBHelper.fetchRestaurantByCuisineAndNeighborhood(cuisine, neighborhood, (error, restaurants) => {
+    if (error) { // Got an error!
+      console.error(error);
+    } else {
+      resetRestaurants(restaurants);
+      fillRestaurantsHTML();
+    }
+  });
+};
+
+/**
+ *  creates static map image
+ */
+const staticMapImage = (height, element) => {
+  const width =
+    window.innerWidth ||
+    document.documentElement.clientWidth ||
+    document.body.clientWidth;
+
+  const url = oneLineTrim`
+    https://maps.googleapis.com/maps/api/staticmap?center=40.722216,+-73.987501&
+    zoom=12&
+    scale=2&
+    size=${width}x${height}&
+    maptype=roadmap&format=png&
+    visual_refresh=true&
+    key=AIzaSyAI60PBarZdCiO-BYJqYvoDYBL8F68-PEU&
+    markers=size:mid%7Ccolor:red%7C
+    |40.683555,-73.966393|
+    |40.713829,-73.989667|
+    |40.747143,-73.985414|
+    |40.722216,-73.987501|
+    |40.705089,-73.933585|
+    |40.674925,-74.016162|
+    |40.727397,-73.983645|
+    |40.726584,-74.002082|
+    |40.743797,-73.950652|
+    |40.743394,-73.954235|
+    `;
+
+  const mapsImage = `
+  <img width="${width}px"
+  src=${encodeURI(url)} alt="Map of all restaurants" id="mapImage">
+  `;
+  document.getElementById(`${element}`).innerHTML = mapsImage;
+};
+
+const addInteractiveMap = () => {
+  loadGoogleMapsApi({ key: 'AIzaSyB4KP3fH_OO_RnMVzIQ6_w5JEPQZsbue3M', libraries: ['places'] })
+    .then((googleMaps) => {
+      globalMap = new googleMaps.Map(document.querySelector('#map'), {
+        zoom: 12,
+        center: {
+          lat: 40.722216,
+          lng: -73.987501
+        },
+        scrollwheel: false
+      });
+      globalMapLoaded = true;
+      titleGoogleMap(globalMap, 'Restaurants Map');
+      updateRestaurants();
+    }).catch((error) => {
+      console.error(error);
+    });
+};
+
+const initMap = (height, element) => {
+  // load static maps image for smaller devices
+  if (window.matchMedia('(max-width:600px)').matches) {
+    staticMapImage(height, element);
+    document.getElementById(element).addEventListener(
+      'mouseover',
+      () => {
+        if (!globalMapLoaded) {
+          addInteractiveMap();
+        }
+      },
+      { once: true }
+    );
+  } else {
+    addInteractiveMap();
+  }
+};
+
+/**
+ * Fetch all cuisines and set their HTML.
+ */
+const fetchCuisines = () => {
+  DBHelper.fetchCuisines((error, newItems) => {
+    if (error) { // Got an error!
+      console.error(error);
+    } else {
+      globalCuisines = newItems;
+      fillCuisinesHTML();
+    }
+  });
+};
+
+/**
+ * Fetch all neighborhoods and set their HTML.
+ */
+const fetchNeighborhoods = () => {
+  DBHelper.fetchNeighborhoods((error, newItems) => {
+    if (error) { // Got an error
+      console.error(error);
+    } else {
+      globalNeighborhoods = newItems;
+      fillNeighborhoodsHTML();
+    }
+  });
+};
+
+/**
+ * Fetch neighborhoods and cuisines as soon as the page is loaded.
+ */
+document.addEventListener('DOMContentLoaded', () => {
+  initMap(400, 'map');
+  fetchNeighborhoods();
+  fetchCuisines();
+  document.getElementById('neighborhoods-select').addEventListener('change', updateRestaurants);
+  document.getElementById('cuisines-select').addEventListener('change', updateRestaurants);
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,68 +1,135 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const WebpackMd5Hash = require('webpack-md5-hash');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const DashboardPlugin = require("webpack-dashboard/plugin");
+const DashboardPlugin = require('webpack-dashboard/plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const autoprefixer = require('autoprefixer');
+const responsiveLoaderSharp = require('responsive-loader/sharp');
+const HtmlCriticalWebpackPlugin = require('html-critical-webpack-plugin');
+
 const devMode = process.env.NODE_ENV !== 'production';
 
 module.exports = {
   mode: 'development',
   entry: {
-    main: "./app/js/main.js",
-    restaurant: "./app/js/restaurant.js"
+    main: './app/js/main.js',
+    restaurant: './app/js/restaurant.js'
   },
   output: {
+    path: path.resolve(__dirname, 'dist'),
     filename: 'js/[name].js',
   },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new UglifyJsPlugin({
+        include: /\.min\.js$/
+      })
+    ]
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+    port: 8000,
+    quiet: true,
+    watchContentBase: true
+  },
+  plugins: [
+    new WebpackMd5Hash(),
+    new CleanWebpackPlugin('dist'),
+    new UglifyJsPlugin({
+      test: /\.js$/,
+      cache: true,
+      sourceMap: devMode,
+      extractComments: !devMode
+    }),
+    new CopyWebpackPlugin([
+      {
+        from: './data/**/*.json',
+        to: '.',
+        flatten: true,
+        context: './app/'
+      }
+    ]),
+    new CopyWebpackPlugin([
+      {
+        from: './img/icons/*',
+        to: './img/icons',
+        flatten: true,
+        context: './app/'
+      }
+    ]),
+    new DashboardPlugin(),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './app/index.html',
+      chunks: ['main']
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'restaurant.html',
+      template: './app/restaurant.html',
+      chunks: ['restaurant']
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'css/styles.css'
+    }),
+    new HtmlCriticalWebpackPlugin({
+      base: 'dist',
+      src: 'index.html',
+      dest: 'index.html',
+      inline: true,
+      minify: true,
+      extract: true,
+      width: 1300,
+      height: 900,
+      penthouse: {
+        blockJSRequests: false,
+      }
+    })
+  ],
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets: ["env"]
+            presets: ['env']
           }
         }
-      }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: () => [autoprefixer()]
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(jpg|png)$/,
+        use: [
+          {
+            loader: 'responsive-loader',
+            options: {
+              adapter: responsiveLoaderSharp,
+              sizes: [200, 500, 630, 800],
+              placeholder: true,
+              placeholderSize: 50
+            }
+          }
+        ]
+      },
     ]
-  },
-  devServer: {
-    contentBase: "./dist",
-    port: 8000,
-    quiet: true,
-    watchContentBase: true
-  },
-  devtool: "source-map",
-  plugins: [
-    new CleanWebpackPlugin("dist"),
-    // new UglifyJsPlugin({
-    //   test: /\.js$/,
-    //   cache: true,
-    //   sourceMap: true
-    // }),
-    new CopyWebpackPlugin([
-      {from: './data/**/*.json', to: '.', flatten: true, context: './app/'},
-      {from: './css/**/*.css', to: '.', context: './app/'}
-    ]),
-    new DashboardPlugin(),
-    new HtmlWebpackPlugin({
-      filename: "index.html",
-      template: "./app/index.html",
-      chunks: ["main"]
-    }),
-    new HtmlWebpackPlugin({
-      filename: "restaurant.html",
-      template: "./app/restaurant.html",
-      chunks: ["restaurant"]
-    }),
-    new MiniCssExtractPlugin({
-      // Options similar to the same options in webpackOptions.output
-      // both options are optional
-      filename: devMode ? '[name].css' : '[name].[hash].css',
-      chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
-    })
-  ]
+  }
 };
